b7a54889e961a0e00f573f767274607f9c09357a
stevenbai0724 <stevenbai0724@gmail.com>
2021-08-10 10:21:20 -0400

    8/10/21

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('BLEACHUGGR BOT IS ONLINE');
+});
+
+
+
+
+client.login('ODc0NjUzMDc0OTE0OTU1MzA0.YRKGJw.52HhvbX0yhfVGrhbwU9RPimHL0s');
